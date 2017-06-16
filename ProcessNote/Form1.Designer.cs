8113d01f3bdd3b2e70e0a0a56bee4f41bad168ce
vetesipeti <vetesipeti@gmail.com>
2017-06-16 06:13:25 +0200

    half done

---
+++ ProcessNote/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ProcessNote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.totalCpu = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.totalMemory = new System.Windows.Forms.ToolStripStatusLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(386, 320);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process name";
+            this.columnHeader1.Width = 121;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            this.columnHeader2.Width = 68;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Memory usage";
+            this.columnHeader3.Width = 92;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "CPU usage";
+            this.columnHeader4.Width = 102;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.totalCpu,
+            this.toolStripStatusLabel2,
+            this.totalMemory});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 323);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(780, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(97, 17);
+            this.toolStripStatusLabel1.Text = "Total CPU usage:";
+            // 
+            // totalCpu
+            // 
+            this.totalCpu.Name = "totalCpu";
+            this.totalCpu.Size = new System.Drawing.Size(54, 17);
+            this.totalCpu.Text = "-              ";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(122, 17);
+            this.toolStripStatusLabel2.Text = "Total memory usage: ";
+            // 
+            // totalMemory
+            // 
+            this.totalMemory.Name = "totalMemory";
+            this.totalMemory.Size = new System.Drawing.Size(51, 17);
+            this.totalMemory.Text = "-             ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(676, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Always on top";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(402, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(255, 121);
+            this.listBox1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(402, 165);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(255, 155);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(402, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Comment:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 345);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Process Note";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolStripStatusLabel totalCpu;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel totalMemory;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ProcessNote/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ProcessNote
+{
+    public partial class Form1 : Form
+    {
+        PerformanceCounter totalCpuCounter;
+        PerformanceCounter totalRamCounter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitPerformanceCounters()
+        {
+            totalCpuCounter = new PerformanceCounter
+               ("Processor", "% Processor Time", "_Total");
+            totalRamCounter = new PerformanceCounter
+                ("Memory", "Available MBytes");
+        }
+        
+
+        public string BytesToReadableValue(long number)
+        {
+            List<string> suffixes = new List<string> { " B", " KB", " MB", " GB", " TB", " PB" };
+
+            for (int i = 0; i < suffixes.Count; i++)
+            {
+                long temp = number / (int)Math.Pow(1024, i + 1);
+
+                if (temp == 0)
+                {
+                    return (number / (int)Math.Pow(1024, i)) + suffixes[i];
+                }
+            }
+
+            return number.ToString();
+        }
+    
+        public void RenderProcessesOnListView()
+        {
+            Process[] processList = Process.GetProcesses();
+            foreach (Process process in processList)
+            {
+                string[] attributes =
+                {
+                    process.ProcessName,
+                    process.Id.ToString(),
+                    BytesToReadableValue(process.PrivateMemorySize64),                    
+                };
+                ListViewItem item = new ListViewItem(attributes);
+                listView1.Items.Add(item);
+
+            }            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitPerformanceCounters();
+            RenderProcessesOnListView();
+            timer1.Interval = 300;
+            timer2.Interval = 1000;
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = !TopMost;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //String.Format("{0:##0} %", cpuCounter.NextValue());
+            totalMemory.Text = String.Format("{0} GB",Math.Round(7.9 - (totalRamCounter.NextValue()/1000), 1).ToString());
+            Process[] processList = Process.GetProcesses();
+            List<string> processIds = new List<string>();
+            List<string> listViewProcessIds = new List<string>();
+
+            foreach (Process process in processList) processIds.Add(process.Id.ToString());
+            foreach (ListViewItem item in listView1.Items) listViewProcessIds.Add(item.SubItems[1].Text);
+
+            if (processList.Length == listView1.Items.Count)
+            {
+                foreach (Process process in processList)
+                {
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        if (item.SubItems[1].Text == process.Id.ToString() && item.SubItems[2].Text != BytesToReadableValue(process.PrivateMemorySize64))
+                        {
+                            item.SubItems[2].Text = BytesToReadableValue(process.PrivateMemorySize64);
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (processList.Length > listView1.Items.Count)
+            {
+                foreach (Process process in processList)
+                {
+                    if (!listViewProcessIds.Contains(process.Id.ToString()))
+                    {
+                        string[] attributes = {
+                            process.ProcessName,
+                            process.Id.ToString(),
+                            BytesToReadableValue(process.PrivateMemorySize64),
+                        };
+                        ListViewItem newItem = new ListViewItem(attributes);
+                        listView1.Items.Add(newItem);
+                    }
+
+                }
+            }
+            else
+            {
+                foreach (ListViewItem item in listView1.Items)
+                {
+                    if (!processIds.Contains(item.SubItems[1].Text)) listView1.Items.Remove(item);
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            totalCpu.Text = Math.Round(totalCpuCounter.NextValue(), 0).ToString() + " %";  
+        }
+    }
+}
